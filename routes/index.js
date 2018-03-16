ea5b4584fcfaf0b4eb289dc338e211518165bb81
lixiang <lixiang@126.com>
2018-03-16 14:11:35 +0800

    #mysql

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+router.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://192.168.101.32:8000");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
